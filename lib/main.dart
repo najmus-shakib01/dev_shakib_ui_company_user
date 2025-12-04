64ba7ec4ebae626e1895b1469215588d99dc6075
najmus-shakib01 <syednajmusshakib130@gmail.com>
2025-12-04 19:44:37 +0600

    fix(img): company_user sarah.png added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-// import 'package:dev_shakib_ui_customer/Company_User/customer_list.dart';
+import 'package:dev_shakib_ui_customer/Company_User/customer_list.dart';
 // import 'package:dev_shakib_ui_customer/Company_User/crm_pipeline.dart';
 // import 'package:dev_shakib_ui_customer/Company_User/customer_detail_screen.dart';
 // import 'package:dev_shakib_ui_customer/Company_User/profile_and_setting.dart';
 // import 'package:dev_shakib_ui_customer/Company_User/referral_management_dashboard.dart';
-import 'package:dev_shakib_ui_customer/Company_User/create_user_admin.dart';
+// import 'package:dev_shakib_ui_customer/Company_User/create_user_admin.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,12 +18,12 @@ class ReferableApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Company User Referral App',
-      // home: const CustomerListPage(),
+      home: const CustomerListPage(),
       // home: const CrmPipelineApp(),
       // home: const CustomerDetailApp(),
       // home: const ProfileAndSettingApp(),
       // home: const ReferableManagementDashboard(),
-      home: const CreateUserAdminPage(),
+      // home: const CreateUserAdminPage(),
     );
   }
 }
